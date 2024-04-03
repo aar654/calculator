aa578d8f981636be2de954a7b6f8b49eea8cd008
Aaron Smith <smithaaron654@gmail.com>
2024-04-03 16:30:16 -0400

    added backspace function to complete calculator

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const display = document.getElementById('equation');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
 
 let firstNumber;
 let currentOperator;
@@ -45,25 +46,25 @@ function calculate() {
     secondNumber = Number(tempArray.join(""));
     let result = operate(parseFloat(firstNumber), parseFloat(secondNumber), currentOperator);
     firstNumber = result; // Store result to firstNumber
-    
+
     console.log('the result is ' + firstNumber);
     display.textContent = firstNumber;
 }
 
 //operator functions
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     if (b === 0)
         return "Can't divide by 0";
 
@@ -76,18 +77,23 @@ function divide(a,b) {
 //operator functions based on operator
 function operate(num1, num2, operator) {
     switch (operator) {
-      case '+':
-        return add(num1, num2)
-      case '-':
-        return subtract(num1, num2)
-      case '*':
-        return multiply(num1, num2)
-      case '/':
-        if (num2 === 0) return "Error"
-        else return divide(num1, num2)
-      default:
-        return null;
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return subtract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            if (num2 === 0) return "Error"
+            else return divide(num1, num2)
+        default:
+            return null;
     }
 }
+
+backspace.addEventListener('click', () => {
+    tempArray.pop(); // Remove the last element from tempArray
+    display.textContent = tempArray.length === 0 ? '0' : tempArray.join(""); // Update the display
+});
 
 equalsKey.addEventListener('click', calculate);
